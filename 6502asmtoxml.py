12bd0c0fc41da968f43c11d29bff47875a163783
Jamie Little <robojamie@gmail.com>
2012-12-06 20:32:51 -0600

    Continuing work on conversion script

--- 6502asmtoxml.py
+++ 6502asmtoxml.py
@@ -5,6 +5,7 @@ from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 
 
 comment_list = []
+line_list = []
 
 def convert_asm(asm_file):
     asm_file = open(asm_file, "r")
@@ -13,13 +14,23 @@ def convert_asm(asm_file):
         if line[0] == ";":
             comment_list.append(line)
            
+        if line[0] == ' ':
+            line_list.append(line[1:5])
             
+            
+
 def make_xml():
     root = ET.Element('sixfivezerotwo')
 
     if comment_list:
         multiLineComment = ET.SubElement(root, 'multiLineComment')
         multiLineComment.text = ''.join(comment_list)
+
+    if line_list:
+        for line in line_list:
+            lineNode = ET.SubElement(root, 'line')
+            mnemonicNode = ET.SubElement(lineNode, 'mnemonic')
+            mnemonicNode.text = line        
 
     tree = ET.ElementTree(root)
     
